c8771b82eddd5bee1c1ecf808122ad4e79e83fa6
Daramola Daniel <dandaramola.d@gmail.com>
2023-11-11 16:21:02 +0100

    update_change

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -31,7 +31,7 @@ const Signin = () => {
           <input type="email" placeholder='Email...' onChange={(e) => setEmail(e.target.value)}/>
           <input type="password" placeholder='Password...' onChange={(e) => setPassword(e.target.value)}/>
           <button type="submit">Sign in</button>
-          <p>Don't have an account? <Link href="/signup">Sign up</Link></p>
+          <p>Don&apos;t have an account? <Link href="/signup">Sign up</Link></p>
         </form>
       </div>
     </div>
